a733aba8e627f697dcc761f96f3c1a9416e0258e
Piloniak <xxziomcioxx@gmail.com>
2024-07-28 17:38:28 +0200

    Creating classes for sql

---
+++ Kursy Betclic/Models/Fight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursy_Betclic
+{
+    class Fight
+    {
+        public int Id { get; set; }
+        public double OddsOfWinning { get; set; }
+        public bool haveWon { get; set; }
+        public bool wasFavourite { get; set; }
+    }
+}
---
+++ Kursy Betclic/Models/Fighter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Kursy_Betclic.Models
+{
+    class Fighter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        ObservableCollection<Fight> Fights { get; set; }
+    }
+    class DBFighterClass
+    {
+        [PrimaryKey, AutoIncrement, SQLite.Column("Id")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
