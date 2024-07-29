d3946c0945af497f4f5a9de165b9d6c7a7a2364b
Piloniak <xxziomcioxx@gmail.com>
2024-07-29 18:23:07 +0200

    Layout update

---
+++ Kursy Betclic/Constants.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursy_Betclic
+{
+    public static class Constants
+    {
+        public const string DataBaseFileName = "Odds.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DataBasePath =>
+            Path.Combine(System.AppContext.BaseDirectory, DataBaseFileName);
+    }
+
+    
+}
